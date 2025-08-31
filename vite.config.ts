13e1c4d3354f3f3f18a139c6cbaab41f3f6655f5
evsey9 <evseyru1@mail.ru>
2025-08-31 19:55:42 +0500

    fix2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+	base: '/aegisai/',        // ← имя твоего репозитория на GitHub
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
