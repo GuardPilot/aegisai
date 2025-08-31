ad12254f5301818a3edb7f08cda53467a5f988fb
JV <46264008+GuardPilot@users.noreply.github.com>
2025-08-31 14:44:46 +0300

    Update and rename index.html to ex.h

---
+++ ex.h
@@ -0,0 +1,8 @@
+
+
+                onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+                className={navStyles.mobileMenuButton}
+                aria-label={t('aria.toggleMobileMenu')}
+                aria-expanded={isMobileMenuOpen}
+           
+     
