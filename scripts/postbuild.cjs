4fea91d83168d60a02300322478118532f1b9e0a
JV <46264008+GuardPilot@users.noreply.github.com>
2025-08-31 22:02:49 +0300

    Delete scripts/postbuild.cjs

--- scripts/postbuild.cjs
+++
@@ -1,5 +0,0 @@
-// scripts/postbuild.cjs
-const fs = require('fs');
-fs.copyFileSync('docs/index.html', 'docs/404.html'); // SPA 404 fallback
-fs.writeFileSync('docs/.nojekyll', '');              // отключить Jekyll
-console.log('Postbuild: 404.html и .nojekyll созданы.');
